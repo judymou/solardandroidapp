aa66102290fe54ad50672e3008de904cee2d3c3e
judyMou <moujudy@gmail.com>
2012-11-08 23:32:04 -0800

    Added buttons

--- src/com/caltech/edu/dalehouseapp/MainActivity.java
+++ src/com/caltech/edu/dalehouseapp/MainActivity.java
@@ -3,13 +3,52 @@ package com.caltech.edu.dalehouseapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private Button b1;
+	private Button b2;
+	private Button b3;
+	private Button b4;
+	private Button b5;
+	private Button b6;
+	private Boolean b1_b = false;
+	private Boolean b2_b = false;
+	private Boolean b3_b = false;
+	private Boolean b4_b = false;
+	private Boolean b5_b = false;
+	private Boolean b6_b = false;
+	
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				if (b1_b == false) {
+					Toast.makeText(getApplicationContext(), "TV is on", Toast.LENGTH_SHORT).show();
+					b1_b = true;
+				} else {
+					Toast.makeText(getApplicationContext(), "TV is off", Toast.LENGTH_SHORT).show();
+					b1_b = false;
+				}
+			}
+        	
+        });
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
+        b5 = (Button) findViewById(R.id.button5);
+        b6 = (Button) findViewById(R.id.button6);
     }
 
     @Override
@@ -17,4 +56,6 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    
 }
